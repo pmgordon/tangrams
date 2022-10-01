3d99ea6c92b1c772b11fae4157f6131412e40172
Paul Gordon <paul@Pauls-MacBook-Pro.local>
2022-10-01 14:51:05 -0500

    feat: making figures less blurry

--- experiments/tangrams_sequential/stimuli/objectSet.js
+++ experiments/tangrams_sequential/stimuli/objectSet.js
@@ -2,29 +2,29 @@
 
 // TANGRAM OBJECTS
 var tangramA = {
-	url: 'stimuli/tangram_A.png', name: "A", width: 282.1, height: 275.1}
+	url: 'stimuli/tangram_A_2.png', name: "A", width: 282.1, height: 275.1}
 var tangramB = {
-	url: 'stimuli/tangram_B.png', name: "B", width : 235.2, height: 224}
+	url: 'stimuli/tangram_B_2.png', name: "B", width : 235.2, height: 224}
 var tangramC = {
-	url: 'stimuli/tangram_C.png', name: "C", width: 293.3, height: 228.9}
+	url: 'stimuli/tangram_C_2.png', name: "C", width: 293.3, height: 228.9}
 var tangramD = {
-	url: 'stimuli/tangram_D.png', name: "D", width: 189, height: 271.6}
+	url: 'stimuli/tangram_D_2.png', name: "D", width: 189, height: 271.6}
 var tangramE = {
-	url: 'stimuli/tangram_E.png', name: 'E', width: 204.5, height: 250}
+	url: 'stimuli/tangram_E_2.png', name: 'E', width: 204.5, height: 250}
 var tangramF = {
-	url: 'stimuli/tangram_F.png', name: 'F', width: 261.8, height: 219.8}
+	url: 'stimuli/tangram_F_2.png', name: 'F', width: 261.8, height: 219.8}
 var tangramG = {
-	url: 'stimuli/tangram_G.png', name: 'G', width: 284.9, height: 237.3}
+	url: 'stimuli/tangram_G_2.png', name: 'G', width: 284.9, height: 237.3}
 var tangramH = {
-	url: 'stimuli/tangram_H.png', name: 'H', width: 175, height: 273.7}
+	url: 'stimuli/tangram_H_2.png', name: 'H', width: 175, height: 273.7}
 var tangramI = {
-	url: 'stimuli/tangram_I.png', name: 'I', width: 222.5, height: 247}
+	url: 'stimuli/tangram_I_2.png', name: 'I', width: 222.5, height: 247}
 var tangramJ = {
-	url: 'stimuli/tangram_J.png', name: 'J', width: 151.8, height: 249.2}
+	url: 'stimuli/tangram_J_2.png', name: 'J', width: 151.8, height: 249.2}
 var tangramK = {
-	url: 'stimuli/tangram_K.png', name: 'K', width: 184.8, height: 280}
+	url: 'stimuli/tangram_K_2.png', name: 'K', width: 184.8, height: 280}
 var tangramL = {
-	url: 'stimuli/tangram_L.png', name: 'L', width: 182.7, height: 280.7}
+	url: 'stimuli/tangram_L_2.png', name: 'L', width: 182.7, height: 280.7}
 
 var tangramList = [
 	tangramA, tangramB, tangramC, tangramD, tangramE, tangramF, 
